d30adb690757f37ad5efa97f7f5f85007f037e83
Jon Fortescue <jofortes@microsoft.com>
2021-03-08 13:27:16 -0800

    Remove resxs; xlf only now

--- eng/common/generate-locproject.ps1
+++ eng/common/generate-locproject.ps1
@@ -9,22 +9,21 @@ if (Test-Path -Path $exclusionsFilePath)
 }
 
 Push-Location "$env:BUILD_SOURCESDIRECTORY" # push location for Resolve-Path -Relative to work
-$resxFiles = Get-ChildItem -Recurse -Path "$env:BUILD_SOURCESDIRECTORY\*\*.resx"
 $xlfFiles = @()
 
-# $allXlfFiles = Get-ChildItem -Recurse -Path "$env:BUILD_SOURCESDIRECTORY\*\*.xlf"
-# $langXlfFiles = @()
-# if ($allXlfFiles.Length -gt 0) {
-#     $allXlfFiles[0].FullName -Match "\.([\w-]+)\.xlf"
-#     $firstLangCode = $Matches.1
-#     $langXlfFiles = Get-ChildItem -Recurse -Path "$env:BUILD_SOURCESDIRECTORY\*\*.$firstLangCode.xlf"
-# }
-# $langXlfFiles | ForEach-Object {
-#     $_.Name -Match "([^.]+)\.[\w-]+\.xlf"
-#     $xlfFiles += Copy-Item "$($_.FullName)" -Destination "$($_.Directory.FullName)\$($Matches.1).xlf" -PassThru
-# }
+$allXlfFiles = Get-ChildItem -Recurse -Path "$env:BUILD_SOURCESDIRECTORY\*\*.xlf"
+$langXlfFiles = @()
+if ($allXlfFiles.Length -gt 0) {
+    $allXlfFiles[0].FullName -Match "\.([\w-]+)\.xlf"
+    $firstLangCode = $Matches.1
+    $langXlfFiles = Get-ChildItem -Recurse -Path "$env:BUILD_SOURCESDIRECTORY\*\*.$firstLangCode.xlf"
+}
+$langXlfFiles | ForEach-Object {
+    $_.Name -Match "([^.]+)\.[\w-]+\.xlf"
+    $xlfFiles += Copy-Item "$($_.FullName)" -Destination "$($_.Directory.FullName)\$($Matches.1).xlf" -PassThru
+}
 
-$locFiles = $resxFiles + $xlfFiles
+$locFiles = $xlfFiles
 
 $locJson = @{
     Projects = @(
